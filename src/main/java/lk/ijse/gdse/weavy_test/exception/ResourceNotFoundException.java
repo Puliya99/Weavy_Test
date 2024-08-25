ebb81c093a19569431b3d4be240ebbc78f977f96
lmarcho <lakshitha.mathngadeera@neosolax.com>
2024-08-25 10:22:14 +0530

    handles exception

---
+++ src/main/java/lk/ijse/gdse/weavy_test/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.weavy_test.exception;
+
+public class ResourceNotFoundException extends Exception {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
