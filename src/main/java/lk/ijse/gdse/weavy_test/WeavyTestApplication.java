3fa2eb6e970c27cc11dfd4d47de34f1e1ed374ff
lmarcho <lakshitha.mathngadeera@neosolax.com>
2024-08-25 08:49:36 +0530

    create project

---
+++ src/main/java/lk/ijse/gdse/weavy_test/WeavyTestApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.weavy_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeavyTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse/weavy_test/WeavyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.weavy_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
